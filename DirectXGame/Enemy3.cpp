b27641b64d2b1df5f34ff5b486ad116ade892abd
Tiku-Kouki <k022g0079@g.neec.ac.jp>
2024-02-07 11:19:55 +0900

    enemy追加

---
+++ DirectXGame/Enemy3.cpp
@@ -0,0 +1,9 @@
+#include "Enemy3.h"
+
+void Enemy3::Initialize(const std::vector<Model*>& models) {}
+
+void Enemy3::Update() {}
+
+void Enemy3::Draw(const ViewProjection& viewProjection) {}
+
+void Enemy3::Reset(const Vector3& pos) {}
---
+++ DirectXGame/Enemy3.h
@@ -0,0 +1,37 @@
+#pragma once
+#include "Input.h"
+#include "Model.h"
+#include "WorldTransform.h"
+#include <cassert>
+#include"BaseCharacter.h"
+class Enemy3 : public BaseCharacter {
+
+  public:
+	///< summary>
+	// 初期化
+	///  </summary>
+	void Initialize(const std::vector<Model*>& models) override;
+
+	/// <summary>
+	// 更新
+	/// </summary>
+	void Update() override;
+
+	/// <summary>
+	// 描画
+	/// </summary>
+	void Draw(const ViewProjection& viewProjection) override;
+
+	void Reset(const Vector3& pos);
+
+private:
+	// ワールド変換データ
+	WorldTransform worldTransform_;
+	WorldTransform worldTransformEnemy_Body_;
+	//  モデル
+	Model* model_ = nullptr;
+	// 3Dモデル
+	Model* modelFighterEnemyBody_;
+	float enemySpeed_ = -0.054f;
+
+};
